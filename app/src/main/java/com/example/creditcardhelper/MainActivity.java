f1a283b80051858467d43abd5408433e3476f4c9
talktoyaminipriya <talktoyaminipriya@gmail.com>
2017-09-25 19:24:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/creditcardhelper/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.creditcardhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
